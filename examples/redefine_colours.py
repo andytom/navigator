b461cdddd7e91a27a602fae1ddac992681e95c58
Thomas O'Donnell <andy.tom@gmail.com>
2015-10-17 21:30:39 +0100

    Expanding one of the examples

--- examples/redefine_colours.py
+++ examples/redefine_colours.py
@@ -18,9 +18,16 @@ def colours():
 @nav.route("Change", "try on some different colour scheme")
 def change():
     ui.ColorsFormats.prompt = ui.ColorsFormats.magenta
-    ui.ColorsFormats.info = ui.ColorsFormats.aqua
+    ui.ColorsFormats.info = ui.ColorsFormats.light_blue
     ui.ColorsFormats.success = ui.ColorsFormats.bg_green
     ui.ColorsFormats.error = ui.ColorsFormats.bg_red
+
+@nav.route("Reset", "go back to the old colour scheme")
+def reset():
+    ui.ColorsFormats.prompt = ui.ColorsFormats.yellow
+    ui.ColorsFormats.info = ui.ColorsFormats.light_grey
+    ui.ColorsFormats.success = ui.ColorsFormats.green
+    ui.ColorsFormats.error = ui.ColorsFormats.red
 
 
 if __name__ == "__main__":
